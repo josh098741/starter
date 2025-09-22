62fa4a1d4a96b3c9cc93c2a05b851d2ea874007c
josh098741 <morongemokaya@gmail.com>
2025-09-22 19:10:12 +0300

    Additional commits

--- searchList/src/Search.jsx
+++ searchList/src/Search.jsx
@@ -2,23 +2,25 @@ import React, {useState} from 'react'
 
 function Search(){
 
-    const products = ["laptop", "Phone", "Tablet", "Headphones", "Camera"];
+    const products = ["Apple","Banana","Orange","Grape"];
+
     const [search, setSearch] = useState("")
 
     function handleSetSearch(event){
         setSearch(event.target.value)
     }
 
-    const filteredItems = products.filter((p) => p.toLowerCase().includes(search.toLowerCase()));
+    const filtered = products.filter((p) => p.toLowerCase().includes(search.toLowerCase()))
 
     return(
         <div>
             <input type="text" onChange={handleSetSearch}/>
             <ol>
-                {products.map((product, index) => <li key={index}>{product}</li>)}
+                {products.map((product,index) => <li key={index}>{product}</li>)}
             </ol>
+                
             <ol>
-                {filteredItems.map((product, index) => <li key={index}>{product}</li>)}
+                {filtered.map((product, index) => <li key={index}>{product}</li>)}
             </ol>
         </div>
     );
